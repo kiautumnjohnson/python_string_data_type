a2c6762addc095cbeba5a53b2fb884608eb1ef0d
kiautumnjohnson <113151916+kiautumnjohnson@users.noreply.github.com>
2022-09-27 18:08:53 -0600

    created variables for first and last name

--- python_strings.py
+++ python_strings.py
@@ -1,13 +1,18 @@
 # TODO Create Variables
 #   - Create the following variables
 #   - my_first_name
+
 #       -set this equal to your first name
+my_first_name = 'Kiautumn'
 #   - my_last_name
 #       -set this equal to your last name
+my_last_name = 'Johnson'
 #   - my_year_of_birth
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
+my_birth_year = 1999
 #   - current_year
 #       -set this equal to 2020
+current_year = 2022
 
 
 
