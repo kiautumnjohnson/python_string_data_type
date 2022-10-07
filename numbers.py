96137272a01c356ab4f5df9196ee7fd0180d19a5
kiautumnjohnson <113151916+kiautumnjohnson@users.noreply.github.com>
2022-10-06 18:21:18 -0600

    numbers folder i worked on

---
+++ numbers.py
@@ -0,0 +1,13 @@
+import random
+myInteger = 1923
+secondInteger = 1978
+myRandomYear = random.randint(1974, 2022)
+timePassed = (myRandomYear - myInteger)
+print(timePassed)
+applePrice = 1.79
+orangePrice = 1.25
+
+bananaPriceInt = 2
+bananaPriceFloat = float(bananaPriceInt)
+bill = applePrice + orangePrice + bananaPriceInt
+print(bill)
